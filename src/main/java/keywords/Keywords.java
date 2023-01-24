023ac5f4168883a4ac310dcada2a800f40147e1c
Dawit Wondim <dawit.wondim@aexp.com>
2023-01-23 20:34:15 -0800

    I added a few examples of using the instanceof keyword. Key takeaways, every class is an instance of the Object class EXCEPT null keyword

---
+++ src/main/java/keywords/Keywords.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Keywords {
+
+	public static void main(String [] args) {
+
+	// instanceof keyword
+
+	//Every object is an instance of Object class EXCEPT null
+	
+	String name = "Kobe";
+	String myNullVariable = null;
+	List<Integer> myList = new ArrayList<>(); 
+	System.out.print("String instanceof Object: ");
+	System.out.print(name instanceof Object);
+	System.out.println();
+	System.out.print("ArrayList instanceof Object: ");  
+	System.out.print(myList instanceof Object); 
+	System.out.println();
+	System.out.print("null instanceof Object: ");  
+	System.out.print(myNullVariable instanceof Object);
+	System.out.println();
+	}
+}
